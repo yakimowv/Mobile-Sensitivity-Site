2193e665558fb161b2af5c9ff72e6fd1646e0700
yakimowv <100320246+yakimowv@users.noreply.github.com>
2022-08-14 16:32:10 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {Routes,Route}from 'react-router-dom'
+import Home from './components/HomePage/Home';
+import Services from './components/ServicesPage/Services';
+import SignUp from './components/SignUpPage/SignUp';
+import Products from './components/ProductsPage/Products';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+      <Navbar/>
+    <Routes>
+      <Route path= '/' element={<Home/>} />
+      <Route path= '/services' element={<Services/>} />
+      <Route path= '/sign-up' element={<SignUp/>} />
+      <Route path= '/products' element={<Products/>} />
+      <Route path='*'element={<h1>404 Wrong Page</h1>}/>
+    </Routes>
+    <Footer/>
+
+
+    </>
   );
 }
 
---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,15 @@
+import '../../App.css'
+import VideoSection from '../VideoSection/VideoSection'
+import React from 'react'
+import Card from '../Card/Card'
+
+function Home() {
+  return (
+    <>
+        <VideoSection/>
+        <Card/>
+    </>
+  )
+}
+
+export default Home
